1df63791ecaf07d5785fb7e2fcefa23cbc3a6f06
ShreeSoftech-Bhuj <59550860+ShreeSoftech-Bhuj@users.noreply.github.com>
2021-04-02 14:30:17 +0530

    _

--- Quiz/buttonRotate.swift
+++ Quiz/buttonRotate.swift
@@ -2,14 +2,17 @@ import UIKit
 
 class buttonRotate: UIButton {
     
-    override func awakeFromNib() {    
-        func spinAnimation() -> CABasicAnimation? {
-            let anim = CABasicAnimation(keyPath: "transform.rotation.z")
-            anim.toValue = NSNumber(value: Double.pi * 2.0)
-            anim.duration = CFTimeInterval(1.5)
-            anim.isCumulative = true
-            anim.repeatCount =  .infinity
-            return anim
-        }
+    override func awakeFromNib() {
+        
+        let animation = spinAnimation()
+        self.layer.add(animation!, forKey: "rotationAnimation")
+    }
+    func spinAnimation() -> CABasicAnimation? {
+        let anim = CABasicAnimation(keyPath: "transform.rotation.z")
+        anim.toValue = NSNumber(value: Double.pi * 2.0)
+        anim.duration = CFTimeInterval(1.8)
+        anim.isCumulative = true
+        anim.repeatCount =  .infinity
+        return anim
     }
 }
